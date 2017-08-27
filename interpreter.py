fb8fd20420476cdaad15ca21414738e603f85fb7
Satan's Son <valyrio.email@gmail.com>
2017-08-27 18:22:07 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -436,6 +436,7 @@ EXTENSIONS = {
     '.':lambda i, s: s.push(list(s.pop(i)).index(s.pop())),
     '}':lambda i, s: repeat(s, s.pop(i)),
     '>':lambda i, s: repeat(s.pop(i), s.pop()),
+    '_':lambda i, s: print_(s),
     
     'a':lambda i, s: s.push(PRINTABLE),
     'b':lambda i, s: s.push(ALPHABETU),
